783ccbd4e514feea708543e276397a051093c00f
jul-goher <juliegonzalezhrra@gmail.com>
2025-03-09 14:06:43 -0600

    Data frame final, correciones a las poblaciones para eliminar 0s

--- scripts/diversidad_alfa.R
+++ scripts/diversidad_alfa.R
@@ -1,7 +1,6 @@
 ##########  Datos a tabla
 
 #Diversidad de Shannon, ajuste de Pielou
-
 shan_pielou <- function(x) { {
   n <- length(x)
   total <- sum(x)
@@ -36,7 +35,7 @@ simpson <- function (x) {
   inv_simpson <- (1/simpson)
   
   #índice de Gini-Simpson
-  gini <- (1 - simpson)
+  gini <- (1 - simpson) #No estoy segura si es -inverso o -simpson, dejé el último
   
   #Redondeo de los índices
   r_simpson <- round (simpson, 2)
@@ -64,51 +63,144 @@ chao1 <- function (x) {
  
 }
 
-
-####            Población 1 - perro
-
-pob1_abundancias <- c(18, 4, 4, 10, 15, 5, 3, 3, 2, 2, 1, 1)
-
-#índices
-shan_pielou (pob1_abundancias)
-simpson (pob1_abundancias)
-chao1 (pob1_abundancias)
+#Base de datos 
+#Como está seperado por ";" y no comas, copié el código que aparece al importar el
+datos <- read_delim("data/datos.csv", delim = ";", 
+                    escape_double = FALSE, trim_ws = TRUE)
+View (datos)
 
 
+########## INDICES POR POBLACIÓN  ##########
 
-####            Población 2 - carita feliz
+####            Población 1
+pob1 <- datos[1, 2:13]
+pob1
+
+pob1_shan <- shan_pielou(pob1)
+pob1_shan #Da los valores correctos, pero añade la palabra "marino" al inicio
+
+pob1_simpson <- simpson(pob1)
+pob1_simpson
+
+pob1_chao <- chao1(pob1)
 
 
-####            Población 1 - 
+#Confirmar los índices con vegan
+install.packages("vegan")
+library(vegan)
+shannon_veg_pob1 <- diversity(pob1, index = "shannon")
+shannon_veg_pob1 #son iguales al de la función 
 
+chao1_veg_pob1 <- estimateR(pob1)["S.chao1",]
+chao1_veg_pob1 #La función calcula 13, el de vegan calcula 12.3
 
+####            Población 2 
 
-#################### TABLA DE ÍNDICES ALFA ###########################
+pob2 <- datos[2, 2:13]
+pob2
 
-#Función que haga una data.frame con los índices calculados 
-indices_alfa <- function (x) {
-  data.frame (
-    shannon = shan_pielou(x)$shannon, 
-    pielou = shan_pielou(x)$pielou, 
-    simpson = simpson (x), 
-    chao1 = chao1(x)
+pob2_shan <- shan_pielou(pob2)
+pob2_simpson <- simpson(pob2)
+pob2_chao <- chao1(pob2)
+
+####            Población 3
+pob3 <- datos[3, 2:13]
+#sólo tiene fichas naranjas, así que hay que remover los ceros
+pob3 <- pob3[pob3 > 0]
+pob3
+
+pob3_shan <- shan_pielou(pob3)
+pob3_shan
+pob3_simpson <- simpson(pob3)
+pob3_simpson
+pob3_chao <- chao1(pob3)
+pob3_chao #Da un valor de NaN
+
+####            Población 4
+pob4 <- datos[4, 2:13]
+pob4
+#0 fichas rosas y amarillas, entonces:
+pob4 <- pob4[pob4 > 0]
+pob4
+
+pob4_shan <- shan_pielou(pob4)
+pob4_simpson <- simpson(pob4)
+pob4_chao <- chao1(pob4)
+
+####            Población 5 
+pob5 <- datos[5, 2:13]
+#Eliminar ceros
+pob5 <- pob5[pob5 > 0]
+pob4
+
+pob5_shan <- shan_pielou(pob5)
+pob5_simpson <- simpson(pob5)
+pob5_chao <- chao1(pob5)
+
+####            Población 6 
+pob6 <- datos[6, 2:13]
+#Sólo tiene 66 fichas color pasto
+pob6 <- pob6[pob6 > 0]
+pob6
+
+pob6_shan <- shan_pielou(pob6)
+pob6_simpson <- simpson(pob6)
+pob6_chao <- chao1(pob6)
+
+####            Población 7 
+pob7 <- datos[7, 2:13]
+#Cero fichas amarillas y naranjas
+pob7 <- pob7[pob7 > 0]
+pob7
+
+pob7_shan <- shan_pielou(pob7)
+pob7_simpson <- simpson(pob7)
+pob7_chao <- chao1(pob7)
+
+              #######################################
+############## ÍNDICES ALFA DE TODAS LAS POBLACIONES ####################
+              ######################################
+
+indices_alpha <- data.frame ( list 
+( poblaciones <- c ("P1", "P2", "P3", "P4", "P5", "P6", "P7"), 
+
+indices_shannon <- c ( pob1_shan$shannon, pob2_shan$shannon, pob3_shan$shannon, pob4_shan$shannon,
+                     pob5_shan$shannon, pob6_shan$shannon, pob7_shan$shannon ), 
+
+indices_pielou <- c ( pob1_shan$pielou, pob2_shan$pielou, pob3_shan$pielou, pob4_shan$pielou,
+                     pob5_shan$pielou, pob6_shan$pielou, pob7_shan$pielou ), 
+
+indices_simpson <-c ( pob1_simpson$simpson, pob2_simpson$simpson, pob3_simpson$simpson, pob4_simpson$simpson,
+                      pob5_simpson$simpson, pob6_simpson$simpson, pob7_simpson$simpson ), 
+
+indices_inverso <- c ( pob1_simpson$inv_simpson, pob2_simpson$inv_simpson, pob3_simpson$inv_simpson, pob4_simpson$inv_simpson,
+                       pob5_simpson$inv_simpson, pob6_simpson$inv_simpson, pob7_simpson$inv_simpson ), 
+
+indices_gini <- c ( pob1_simpson$gini, pob2_simpson$gini, pob3_simpson$gini, pob4_simpson$gini,
+                    pob5_simpson$gini, pob6_simpson$gini, pob7_simpson$gini ), 
+
+indices_chao1 <- c ( pob1_chao, pob2_chao, pob3_chao, pob4_chao, pob5_chao, pob6_chao, pob7_chao) 
+ )
+) 
+
+indices_alpha
+
+#Cuando los hago por separado me hace un dta.frame muy confuso
+#Data.frame con los índices calculados 
+indices_alfa <- data.frame ( list (
+  poblaciones = poblaciones,
+  shannon = indices_shannon, 
+  pielou = indices_pielou, 
+  simpson = indices_simpson,
+  inverso_simpson = indices_inverso, 
+  gini = indices_gini, 
+  chao1 = indices_chao1
   )
-}
-indices_alfa(pob1_abundancias)
-
-
-#Prototipo función basurita :p
-pob1 <- data.frame(
-  color = c("azul marino", "azul intermedio", "azul claro", "verde oscuro",
-            "verde claro", "fucsia", "amarillo", "rosa", "naranja", "rojo",
-            "cian", "pastel"),
-  no.fichas = pob1_abundancias, 
-  probabilidad =  for (i in 1:length(x)) { (x[i] / sum(x) )
-  }
-  shannon =  
-    simpson =
-    chao1 = 
 )
+
+print ( indices_alfa )
+
+
 
 
 
