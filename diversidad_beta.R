8bcab9c1f84e9db1aae3ac8dba5707e72aa42b93
blanquis2 <bosornio23@alumnos.uaq.mx>
2025-03-10 00:05:23 -0600

    Borrador beta

--- diversidad_beta.R
+++ diversidad_beta.R
@@ -8,7 +8,7 @@
 data<-read.csv("data/datos.csv")
 data
 
-#Filtrar y transformar a presencia/ausencia
+#Filtrar y transformar a presencia/ausencia con TRUE/FALSE
 
 data1<-data[ ,c(2:13)]>0
 data1
@@ -24,6 +24,8 @@ jaccard<-function(a,b){#Dado el conjunto un conjunto A y uno B
 }
 jaccard(a,b)
 
+####Para Jaccard####
+
 #Hacer una función
 
 jaccard<-function(base){
@@ -37,7 +39,7 @@ for (i in 2:nrow(base)){
 }
 ind_jaccard
 
-ind_jaccard2<-c(0.5694444,1)
+ind_jaccard2<-c(0,1)
 for (a in 3:7) {
   i2<-sum(base[2,] & base[3:nrow(base),]==TRUE)
   u2<-sum(base[a,]| base[3:nrow(base),]==TRUE)
@@ -46,7 +48,7 @@ for (a in 3:7) {
 }
 ind_jaccard2
 
-ind_jaccard3<-c(0.9761905,0.9062500,1)
+ind_jaccard3<-c(0,0,1)
 for (b in 4:7) {
   i3<-sum(base[3,] & base[4:nrow(base),]==TRUE)
   u3<-sum(base[b,]| base[4:nrow(base),]==TRUE)
@@ -56,7 +58,7 @@ for (b in 4:7) {
 ind_jaccard3
 
 
-ind_jaccard4<-c(0.5857143,0.5178571,0.07317073,1)
+ind_jaccard4<-c(0,0,0,1)
 for (c in 5:7) {
   i4<-sum(base[4,] & base[5:nrow(base),]==TRUE)
   u4<-sum(base[c,]| base[5:nrow(base),]==TRUE)
@@ -65,7 +67,7 @@ for (c in 5:7) {
 }
 ind_jaccard4
 
-ind_jaccard5<-c(0.7454545,0.5800000,0.07317073,0.5384615,1)
+ind_jaccard5<-c(0,0,0,0,1)
 for (y in 6:7) {
   i5<-sum(base[5,] & base[6:nrow(base),]==TRUE)
   u5<-sum(base[y,]| base[6:nrow(base),]==TRUE)
@@ -74,7 +76,7 @@ for (y in 6:7) {
 }
 ind_jaccard5
 
-ind_jaccard6<-c(0.9761905 ,0.9062500, 0.09375000, 0.7368421, 0.6666667,1)
+ind_jaccard6<-c(0,0,0,0,0,1)
 for (d in 7) {
   i6<-sum(base[6,] & base[7:nrow(base),]==TRUE)
   u6<-sum(base[d,]| base[7:nrow(base),]==TRUE)
@@ -83,27 +85,45 @@ for (d in 7) {
 }
 ind_jaccard6
 
-ind_jaccard7<-c(0.5857143, 0.5576923, 0.07317073, 0.4375000, 0.4210526, 0.1111111,1)
+ind_jaccard7<-c(0,0,0,0,0,0,1)
 
 mat_jaccard<-cbind(ind_jaccard,ind_jaccard2,ind_jaccard3,ind_jaccard4,ind_jaccard5,ind_jaccard6,ind_jaccard7)
 mat_jaccard
 rownames(mat_jaccard)<-c("p1","p2","p3","p4","p5","p6","p7")
 colnames(mat_jaccard)<-c("p1","p2","p3","p4","p5","p6","p7")
 
-
-##Vamos a usar el paquete devtools para calcular las distancias entre los índices 
-library(usethis)
-library(devtools)
-
 mat_jaccard##Matriz con los índices de Jaccard entre poblaciones
-
-###Matriz de distancias con la función dist que vive en devtools
-mat_dist_jaccard<-dist(mat_jaccard)
-mat_dist_jaccard
-
 }
 
 jaccard(data1)
 
 ##Guardar los datos una carpeta con terminación .csv
-write.csv(mat_dist_jaccard,file = "mat_dist_csv/matriz_distancia_indxJaccard.csv")
+write.csv(mat_jaccard,file = "mat_dist_csv/matriz_distancia_indxJaccard.csv")
+
+
+####Bray-Curtis####
+
+##Con vegdist que vive en vegan 
+
+library(lattice)
+library(permute)
+library(vegan)
+
+#Filtrar
+
+data2<-data[ ,c(2:13)]
+data2
+
+#Matriz
+bd <- vegdist(data2, method = "bray")
+bd
+
+##pre
+data3<-split(data2, 1:nrow(data2))##tengo una lista
+data3
+class(data3)
+
+###Especies comunes
+
+data3$`1`>0 & data3$`2` >0->com
+
